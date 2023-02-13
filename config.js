55093dc42ed4479b24769c17b4fad0687b96118d
sky20000003 <91860173+sky20000003@users.noreply.github.com>
2023-02-13 23:12:49 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '^.^';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Valentine vui vẻ nhé';
+const titleCard = 'Gửi người tớ thích';
+const contentCard = 'Không biết có ai đã tặng cậu quà Valentine chưa, nếu chưa thì đây là món quà của mình , Mong cậu có 1 mùa Valentine vui vẻ, nếu cậu muốn quà khác thì tớ còn mỗi tấm thân này thôi :)).';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl (2).png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
